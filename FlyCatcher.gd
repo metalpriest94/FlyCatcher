9e7b58e5bd712bff48a5b809ce2c8a5462de2e74
metalpriest94 <alexanderb94@gmx.de>
2021-02-13 18:24:26 +0100

    more plant mechanics

--- FlyCatcher.gd
+++ FlyCatcher.gd
@@ -1,33 +1,44 @@
 extends Node2D
-var rigidbody : RigidBody2D
-var onFloor = true;
+var body : KinematicBody2D
+var down_velocity = 0.0
+
+const UP_VECTOR = Vector2(0, -1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	rigidbody = $RigidBody2D
+	body = $KinematicBody2D
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	if Input.is_action_pressed("jump"):
 		jump()
 	if Input.is_action_pressed("walk_left"):
-		walk_left(delta)
+		walk_left()
 	if Input.is_action_pressed("walk_right"):
-		walk_right(delta)
-	print_debug(rigidbody.linear_velocity)
+		walk_right()
+	applyGravity()
+	if (body.is_on_floor()):
+		print_debug("!!!")
 	pass
 
 func jump():
-	if (onFloor):
-		rigidbody.set_axis_velocity(Vector2(rigidbody.linear_velocity.x, -150))
+	if (body.is_on_floor()):
+		body.move_and_slide(Vector2(0, -150), UP_VECTOR)
 
-func walk_right(delta :float):
-	if (onFloor):
-		rigidbody.set_linear_velocity(Vector2(50, rigidbody.linear_velocity.y))
+func walk_right():
+	if (body.is_on_floor()):
+		body.move_and_slide(Vector2(50, 0), UP_VECTOR)
 	
-func walk_left(delta :float):
-	if (onFloor):
-		rigidbody.set_linear_velocity(Vector2(-50, rigidbody.linear_velocity.y))
+func walk_left():
+	if (body.is_on_floor()):
+		body.move_and_slide(Vector2(-50, 0), UP_VECTOR)
+		
+func applyGravity():
+	if (!body.is_on_floor()):
+		down_velocity += 9.81
+		body.move_and_slide_with_snap(Vector2(0, down_velocity), UP_VECTOR)
+	else:
+		down_velocity = 0.0
 	
